52b43b6ca50d77813fdd693ec72de045001458ec
unknown <basma_hashem@ymail.com>
2014-12-16 20:20:26 +0200

    implementation_phase

---
+++ Main.java
@@ -0,0 +1,64 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author basma
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       // new HomePage().setVisible(true);
+       
+        
+
+   
+
+    /**
+     * @param args the command line arguments
+     */
+   
+  
+  reviewFunctiona rf=new reviewFunctiona();
+  int choice;
+  int repeat=1;
+  Scanner in=new Scanner(System.in);
+  
+ 
+      System.out.println("to add a review,comment on a review,like a review (press 1)");
+      choice=in.nextInt();
+    
+   if(choice==1){
+ 
+  new reviewFunctiona().setVisible(true);
+   }
+   if(choice==2){
+      
+   }
+       if(choice==3){
+  
+       }
+
+//  System.out.println("need to do any other proces: 1-yes , 2-no ");
+  // repeat=in.nextInt();
+  }
+    
+  
+    }
+
+
+
+
+        
+    
+
+
---
+++ reviews_controller.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Basma
+ */
+public class reviews_controller  {
+Scanner in=new Scanner(System.in);
+reviews R;
+reviewFunctiona rf;
+public void review(){
+System.out.println("your review has submitted successfully ");
+
+}   
+    public void comment( ){
+System.out.println("your comment has submitted successfully ");
+
+ 
+} 
+public void like( ){
+System.out.println("your action is done ");
+
+} 
+
+}
+
+
+
+
+
